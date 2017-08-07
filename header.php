27d75e4546e52030610c5d938414a03934d5c6d3
kolobok <22zaika22@rambler.ru>
2017-08-08 01:46:45 +0300

    Changed template page - from table to div. Changed footer and header.

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+    <div id="top_slogan"></div>
+    <nav class="container">
+        <div id="logo">
+            <div class="name">ООО &laquo;СКД&raquo;</div>
+            <div class="todo">ошиповка шин для легковых автомобилей</div>
+            <div class="phone">+7 (498) 715-75-15</div>
+            <div class="phone2">+7 (903) 662-80-34</div>
+        </div>
+    </div>
+    <div id="grass"></div>
+    <div class="nav-toggle"><span></span></div>
+    <ul id="menu">
+        <li><a href="./">Главная</a></li>
+        <li><a href="./company.php">О компании</a></li>
+        <li><a href="./price.php">Ценовая информация</a></li>
+        <li><a href="./photo.php">Фотогалерея</a></li>
+        <li><a href="./contacts.php">Контакты</a></li>
+    </ul>
+</nav>
+</header>
--- logo.php
+++
@@ -1,6 +0,0 @@
-<div class="wrapper">
-    <div class="name">ООО &laquo;СКД&raquo;</div>
-    <div class="todo">ошиповка шин для легковых автомобилей</div>
-    <div class="phone">+7 (498) 715-75-15</div>
-    <div class="phone2">+7 (903) 662-80-34</div>
-</div>
