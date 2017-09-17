cc9593db33389b4170c39a857e72c32186b07192
kolobok <22zaika22@rambler.ru>
2017-09-17 23:09:51 +0300

    Changed gallery plugin on Mosaic Flow.

--- photo.php
+++ photo.php
@@ -11,7 +11,6 @@
     <meta content="10 days" name="revisit-after">
     <meta content="index, follow" name="robots">
     <link rel='stylesheet' type='text/css' href='css/main.css' />
-    <link rel='stylesheet' type='text/css' href='css/fotorama.css' />
     <link rel="icon" href="http://<?= $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="http://<?= $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/x-icon">
 </head>
@@ -21,40 +20,76 @@
     ?>
     <div class="container">
         <div class="breadcrumbs"><a href="./" class="breadcrumbs">Главная</a>&nbsp;/&nbsp;Фотогалерея</div>
-        <div class="content">
-            <div id="fotoramawrapper">
-                <div class="fotorama" data-width="960" data-maxwidth="100%" data-ratio="16/9" data-allowfullscreen="true" data-nav="thumbs">
-                    <a href="photogalery/500_333/9.jpg"><img src="photogalery/75_75/9.jpg"></a>
-                    <a href="photogalery/500_333/10.jpg"><img src="photogalery/75_75/10.jpg"></a>
-                    <a href="photogalery/500_333/27.jpg"><img src="photogalery/75_75/27.jpg"></a>
-                    <a href="photogalery/500_333/29.jpg"><img src="photogalery/75_75/29.jpg"></a>
-                    <a href="photogalery/500_333/30.jpg"><img src="photogalery/75_75/30.jpg"></a>
-                    <a href="photogalery/500_333/31.jpg"><img src="photogalery/75_75/31.jpg"></a>
-                    <a href="photogalery/500_333/32.jpg"><img src="photogalery/75_75/32.jpg"></a>
-                    <a href="photogalery/500_333/33.jpg"><img src="photogalery/75_75/33.jpg"></a>
-                    <a href="photogalery/500_333/35.jpg"><img src="photogalery/75_75/35.jpg"></a>
-                    <a href="photogalery/500_333/36.jpg"><img src="photogalery/75_75/36.jpg"></a>
-                    <a href="photogalery/500_333/37.jpg"><img src="photogalery/75_75/37.jpg"></a>
-                    <a href="photogalery/500_333/38.jpg"><img src="photogalery/75_75/38.jpg"></a>
-                    <a href="photogalery/500_333/39.jpg"><img src="photogalery/75_75/39.jpg"></a>
-                    <a href="photogalery/500_333/40.jpg"><img src="photogalery/75_75/40.jpg"></a>
-                    <a href="photogalery/500_333/41.jpg"><img src="photogalery/75_75/41.jpg"></a>
-                    <a href="photogalery/500_333/42.jpg"><img src="photogalery/75_75/42.jpg"></a>
-                    <a href="photogalery/500_333/43.jpg"><img src="photogalery/75_75/43.jpg"></a>
-                    <a href="photogalery/500_333/44.jpg"><img src="photogalery/75_75/44.jpg"></a>
-                    <a href="photogalery/500_333/45.jpg"><img src="photogalery/75_75/45.jpg"></a>
-                    <a href="photogalery/500_333/46.jpg"><img src="photogalery/75_75/46.jpg"></a>
-                    <a href="photogalery/500_333/47.jpg"><img src="photogalery/75_75/47.jpg"></a>
-                    <a href="photogalery/500_333/48.jpg"><img src="photogalery/75_75/48.jpg"></a>
-                </div>
+        <div class="content clearfix mosaicflow">
+            <div class="mosaicflow__item">
+                <img width="400" height="300" src="photogallery/500_333/9.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="200" height="300" src="photogallery/500_333/10.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="400" height="300" src="photogallery/500_333/29.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/30.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/31.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/32.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/33.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/35.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/36.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/37.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/38.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/39.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/40.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/41.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/42.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/43.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/44.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/45.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/46.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/47.jpg" alt="">
+            </div>
+            <div class="mosaicflow__item">
+                <img width="500" height="300" src="photogallery/500_333/48.jpg" alt="">
             </div>
         </div>
     </div>
-</div>
 <?php
 include_once './footer.php';
 ?>
-<script type="text/javascript" src="js/fotorama.js"></script>
+<script src="js/jquery.mosaicflow.min.js"></script>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </body>
 </html>
